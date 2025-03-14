12be6210bb8998376d5127ad61a9996054303c30
Alex Wladimir Salcedo Silva <alex_wss78@hotmail.com>
2025-03-13 21:27:38 -0500

    Initial Commit

---
+++ Drivers/DriversApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriversApp.swift
+//  Drivers
+//
+//  Created by ALEX WLADIMIR SALCEDO SILVA on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DriversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
