2adca6e739eb59b22c380133e52e4c008da2179b
Alex Salcedo <awsalcedo@sri.gob.ec>
2026-03-23 16:50:46 -0500

    Se agrega petición al servidor y manejo de errores.

---
+++ Drivers/RemoteDriversView.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteDriversView.swift
+//  Drivers
+//
+//  Created by usradmin on 23/3/26.
+//
+
+import SwiftUI
+
+struct RemoteDriversView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoteDriversView()
+}
---
+++ Drivers/Services/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  Drivers
+//
+//  Created by usradmin on 23/3/26.
+//
+
+import Foundation
